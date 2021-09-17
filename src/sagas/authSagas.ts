bddb36b3fe48cd90b8b9f5c5de4c2a39c78e5341
Mohammed Islam <mmislam101@gmail.com>
2021-09-17 01:15:39 -0400

    Receive user info in a saga

--- src/sagas/authSagas.ts
+++ src/sagas/authSagas.ts
@@ -1,6 +1,6 @@
 import { put, takeEvery } from 'redux-saga/effects';
 import { getAuthToken } from '../actions';
-import { LOG_IN } from '../constants';
+import { LOG_IN, FETCH_AUTH_SUCCESS } from '../constants';
 
 export function* attemptToLogin(payload: any) {
   const { userInfo } = payload;
@@ -16,4 +16,12 @@ export function* attemptToLogin(payload: any) {
 
 export function* logIn() {
   yield takeEvery(LOG_IN, attemptToLogin);
+}
+
+export function* saveAccessToken(payload: any) {
+  yield console.log('user info', payload.response);
+}
+
+export function* loggedIn() {
+  yield takeEvery(FETCH_AUTH_SUCCESS, saveAccessToken);
 }
--- src/sagas/index.ts
+++ src/sagas/index.ts
@@ -1,9 +1,15 @@
 import { all } from 'redux-saga/effects';
 
 import { appDidLoad, unAuthHydrating } from './appSagas';
-import { logIn } from './authSagas';
+import { logIn, loggedIn } from './authSagas';
 import { fetchTodo } from './countSagas';
 
 export default function* rootSaga() {
-  yield all([logIn(), fetchTodo(), appDidLoad(), unAuthHydrating()]);
+  yield all([
+    logIn(),
+    fetchTodo(),
+    appDidLoad(),
+    unAuthHydrating(),
+    loggedIn(),
+  ]);
 }
