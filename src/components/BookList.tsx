2204926e72cb15182e6632fa88a958a5f1527234
Mohammed Islam <mmislam101@gmail.com>
2021-09-23 21:32:41 -0400

    Implement flatlist with pagination https://aboutreact.com/react-native-flatlist-pagination-to-load-more-data-dynamically-infinite-list/

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -1,25 +1,116 @@
-import React from 'react';
-import { StyleSheet, Text, Button, View } from 'react-native';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchTodos } from '../actions';
+import React, { useState, useEffect } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+  ActivityIndicator,
+} from 'react-native';
+import { makeAPICall } from '../middleware/api';
 
 const styles = StyleSheet.create({
-  booksContainer: {
-    flex: 1,
-    flexDirection: 'column',
+  container: {
     justifyContent: 'center',
+    flex: 1,
+  },
+  footer: {
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  loadMoreBtn: {
+    padding: 10,
+    backgroundColor: '#800000',
+    borderRadius: 4,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  btnText: {
+    color: 'white',
+    fontSize: 15,
+    textAlign: 'center',
   },
 });
 
 const BookList: React.FC = () => {
-  const dispatch = useDispatch();
-  const todo = useSelector((state) => (state as any).count?.todo);
+  const [loading, setLoading] = useState(true);
+  const [dataSource, setDataSource] = useState([]);
+  const [offset, setOffset] = useState(1);
+
+  useEffect(() => getData(), []);
+
+  const getData = () => {
+    console.log('getData');
+    setLoading(true);
+    makeAPICall(`books?page=${offset}`)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log('got data', responseJson);
+        // Successful response
+        setOffset(offset + 1);
+        // Increasing the offset for the next API call
+        setDataSource([...dataSource, ...responseJson.results]);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error('Failed to get books', error);
+      });
+  };
+
+  const renderFooter = () => (
+    // Footer View with Load More button
+    <View style={styles.footer}>
+      <TouchableOpacity
+        activeOpacity={0.9}
+        onPress={getData}
+        // On Click of button load more data
+        style={styles.loadMoreBtn}>
+        <Text style={styles.btnText}>Load More</Text>
+        {loading ? (
+          <ActivityIndicator color="white" style={{ marginLeft: 8 }} />
+        ) : null}
+      </TouchableOpacity>
+    </View>
+  );
+
+  const ItemView = ({ item }) => (
+    // Flat List Item
+    <Text style={styles.itemStyle} onPress={() => getItem(item)}>
+      {item.id}.{item.title.toUpperCase()}
+    </Text>
+  );
+
+  const ItemSeparatorView = () => (
+    // Flat List Item Separator
+    <View
+      style={{
+        height: 0.5,
+        width: '100%',
+        backgroundColor: '#C8C8C8',
+      }}
+    />
+  );
+
+  const getItem = (item) => {
+    // Function for click on an item
+    alert(`Id : ${item.id} Title : ${item.title}`);
+  };
 
   return (
-    <View style={styles.booksContainer}>
-      <Text>Home Screen, only visible once logged in.</Text>
-      <Text>Todo: {todo}</Text>
-      <Button title="Fetch Todo" onPress={() => dispatch(fetchTodos())} />
+    <View style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <FlatList
+          data={dataSource}
+          keyExtractor={(item, index) => index.toString()}
+          ItemSeparatorComponent={ItemSeparatorView}
+          enableEmptySections={true}
+          renderItem={ItemView}
+          ListFooterComponent={renderFooter}
+        />
+      </View>
     </View>
   );
 };
