5e4306e3204a9913ad0b61d3c59cb0268cbce224
Mohammed Islam <mmislam101@gmail.com>
2021-09-30 19:45:21 -0400

    Use effects on progress and overlay

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { View } from 'react-native';
 import { ProgressBar as Bar, Colors } from 'react-native-paper';
 
 const ProgressBar: React.FC = () => {
   const loadingStates = useSelector((state) => (state as any).loading);
-  const displayLoading = Object.keys(loadingStates).reduce(
-    (p, c) => p || loadingStates[c],
-    false,
-  );
 
-  return (
-    <View>{displayLoading && <Bar color={Colors.red800} indeterminate />}</View>
-  );
+  const [displayLoading, setDisplayLoading] = useState(false);
+
+  useEffect(() => {
+    const display = Object.keys(loadingStates).reduce(
+      (p, c) => p || loadingStates[c],
+      false,
+    );
+    setDisplayLoading(display);
+  }, [loadingStates]);
+
+  if (displayLoading) {
+    return <Bar color={Colors.red800} indeterminate />;
+  }
+
+  return null;
 };
 
 export default ProgressBar;
