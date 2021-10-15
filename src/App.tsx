bbf54517ca4d4a341c909f09a118f4d37404dd01
Mohammed Islam <mmislam101@gmail.com>
2021-10-14 22:23:16 -0400

    Simplify loading overlay

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
 import React from 'react';
-import { StatusBar, StyleSheet, View } from 'react-native';
+import { StatusBar } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import LoginContainer from './containers/Login';
 import MainContainer from './containers/Main';
 import { appDidLoad } from './actions';
 import ProgressBar from './components/ProgressBar';
 import Overlay from './components/Overlay';
-
-const styles = StyleSheet.create({
-  overlay: {
-    position: 'absolute',
-    top: 0,
-    right: 0,
-    bottom: 0,
-    left: 0,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
 
 const App: React.FC = () => {
   const isLoggedIn = useSelector((state) => (state as any).auth.isLoggedIn);
@@ -30,9 +18,7 @@ const App: React.FC = () => {
       <StatusBar barStyle="dark-content" />
       <ProgressBar />
       {isLoggedIn ? <MainContainer /> : <LoginContainer />}
-      <View style={styles.overlay}>
-        <Overlay />
-      </View>
+      <Overlay />
     </>
   );
 };
